d33122b3b67d40050cd4cda1e743071800e9107b
u14263 <rbilimova@alfabank.kz>
2022-05-09 23:02:18 +0600

    add database config, entity

---
+++ entity/post.go
@@ -0,0 +1,13 @@
+package entity
+
+type Post struct {
+	ID          uint64 `gorm:"primary_key:auto_increment" json:"id"`
+	Title       string `gorm:"type:varchar(255)" json:"title"`
+	Description string `gorm:"type:text" json:"description"`
+	UserID      uint64 `gorm:"not null" json:"-"`
+	Image       string `gorm:"type:text" json:"image"`
+	Place       string `gorm:"type:text" json:"place"`
+	Category    string `gorm:"type:varchar(255)" json:"category"`
+	Status      string `gorm:"type:varchar(255)" json:"status"`
+	Datetime    string `gorm:"type:varchar(255)" json:"datetime"`
+}
